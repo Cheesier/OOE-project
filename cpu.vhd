d3d1b8dce5ccee8aa0f088012aff259ef2ab4c39
Oscar Thunberg <ostenip@gmail.com>
2014-04-15 19:28:18 +0200

    fixed front portch in rendering, jumps and added layers

--- cpu.vhd
+++ cpu.vhd
@@ -33,7 +33,7 @@ architecture cpu_one of cpu is
     signal rIR : STD_LOGIC_VECTOR(15 downto 0) := X"0300";
     signal rPC : STD_LOGIC_VECTOR(15 downto 0) := X"0000";
     signal rDR : STD_LOGIC_VECTOR(15 downto 0) := X"1111";
-    signal rAR : STD_LOGIC_VECTOR(15 downto 0) := X"0000";
+    signal rAR : STD_LOGIC_VECTOR(15 downto 0) := X"1234";
     signal rHR : STD_LOGIC_VECTOR(15 downto 0) := X"7421";
     signal rSP : STD_LOGIC_VECTOR(15 downto 0) := X"0000";
 
@@ -52,9 +52,11 @@ architecture cpu_one of cpu is
 
     -- Micro memory
     type uMem_type is array (0 to 511) of STD_LOGIC_VECTOR(31 downto 0);
-    constant uMem : uMem_type := (0=>X"01580000", 
-                                  1=>X"08500000",
-                                  2=>X"07580000",
+    constant uMem : uMem_type := (0=>X"00000000", 
+                                  1=>X"05800000",
+                                  2=>X"07880000",
+                                  3=>X"08780000",
+                                  4=>X"00002000",
                             others=> X"00000000");
 
     -- uPC
@@ -79,14 +81,14 @@ architecture cpu_one of cpu is
     signal old_step : STD_LOGIC := '0';
 
 begin
-    led_driver: leddriver port map (clk, rst, seg, an, led, rDR, uPC(7 downto 0));
+    led_driver: leddriver port map (clk, rst, seg, an, led, rHR, uPC(7 downto 0));
 
     -- *****************************
     -- * CONTROL UNIT              *
     -- *****************************
     process(clk) begin
         if rising_edge(clk) then
-
+            
             -- rst
             if rst = '1' then
                 rPC <= X"0000";
@@ -105,21 +107,6 @@ begin
                     when others => null;
                 end case;
 
-                -- TO BUS
-                case uMem(conv_integer(uPC))(27 downto 24) is
-                    when "0001" => databus <= rASR;
-                    when "0010" => databus <= rIR;
-                    when "0011" => null; -- PM, FIX PLZ
-                    when "0100" => databus <= rPC;
-                    when "0101" => databus <= rDR;
-                    --when "0110" => databus <= uMem(conv_integer(uPC));
-                    when "0111" => databus <= rAR;
-                    when "1000" => databus <= rHR;
-                    when "1001" => databus <= rSP;
-                    --when "1010" => databus <= rGR; -- GR mux
-                    --when "1011" => databus <= rVR; -- VR mux
-                    when others => null;
-                end case;
 
                 -- FROM BUS
                 case uMem(conv_integer(uPC))(23 downto 20) is
@@ -143,9 +130,46 @@ begin
                     when "00001" => uPC <= K1(conv_integer(rIR(15 downto 10)));
                     when "00010" => uPC <= K2(conv_integer(rIR(9 downto 8)));
                     when "00011" => uPC <= "000000000";
+                    when "10000" => uPC <= uMem(conv_integer(uPC))(8 downto 0);
+                    --when "10001" and fZ = '1' => uPC <= uMem(conv_integer(uPC))(8 downto 0);
+                    --when "10010" and fZ = '0' => uPC <= uMem(conv_integer(uPC))(8 downto 0);
+                    --when "10011" and fN = '1' => uPC <= uMem(conv_integer(uPC))(8 downto 0);
+                    --when "10100" and fN = '0' => uPC <= uMem(conv_integer(uPC))(8 downto 0);
+                    --when "10101" and fC = '1' => uPC <= uMem(conv_integer(uPC))(8 downto 0);
+                    --when "10110" and fC = '0' => uPC <= uMem(conv_integer(uPC))(8 downto 0);
+                    --when "10111" and fO = '1' => uPC <= uMem(conv_integer(uPC))(8 downto 0);
+                    --when "11000" and fO = '0' => uPC <= uMem(conv_integer(uPC))(8 downto 0);
+                    --when "11001" and fL = '1' => uPC <= uMem(conv_integer(uPC))(8 downto 0);
+                    --when "11010" and fL = '0' => uPC <= uMem(conv_integer(uPC))(8 downto 0);
+                    --when "11011" and fV = '1' => uPC <= uMem(conv_integer(uPC))(8 downto 0);
+                    --when "11100" and fV = '0' => uPC <= uMem(conv_integer(uPC))(8 downto 0);
+                    when "11101" => 
+                        uPC <= uMem(conv_integer(uPC))(8 downto 0),
+                        SuPC <= uPC+1;
+                    when "11110" => uPC <= SuPC;
                     when others => null;
                 end case;
             end if;
         end if;
-    end process;    
+    end process;
+    
+    -- TO BUS
+    with uMem(conv_integer(uPC))(27 downto 24) select
+	databus <= rASR when "0001",
+			   rIR when "0010",
+			   --PM when "0011",
+               rPC when "0100",
+               rDR when "0101",
+               --uMem(conv_integer(uPC)) when "0110",
+               rAR when "0111",
+               rHR when "1000",
+               rSP when "1001",
+               --rGR when "1010",
+               --rVR when "1011",
+               X"0000" when others;
+
+    -- *****************************
+    -- * ALU - TODO                *
+    -- *****************************
+
 end cpu_one;
--- gpu.vhd
+++ gpu.vhd
@@ -11,17 +11,17 @@ entity gpu is
 end gpu;
 
 architecture gpu_one of gpu is
-    signal xctr,yctr : STD_LOGIC_VECTOR(9 downto 0) := "0000000000";
-    alias xtile : STD_LOGIC_VECTOR(5 downto 0) is xctr(9 downto 4);
-    alias ytile : STD_LOGIC_VECTOR(5 downto 0) is yctr(9 downto 4);
-    alias tilexoff : STD_LOGIC_VECTOR(3 downto 0) is xctr(3 downto 0);
-    alias tileyoff : STD_LOGIC_VECTOR(3 downto 0) is yctr(3 downto 0);
+    signal xctr,yctr : STD_LOGIC_VECTOR(10 downto 0) := "00000000000";
+    alias xtile : STD_LOGIC_VECTOR(5 downto 0) is xctr(10 downto 5);
+    alias ytile : STD_LOGIC_VECTOR(5 downto 0) is yctr(10 downto 5);
+    alias tilexoff : STD_LOGIC_VECTOR(3 downto 0) is xctr(4 downto 1);
+    alias tileyoff : STD_LOGIC_VECTOR(3 downto 0) is yctr(4 downto 1);
     signal hs : STD_LOGIC := '1';
     signal vs : STD_LOGIC := '1';
     signal pixel : STD_LOGIC_VECTOR(1 downto 0) := "00";
     signal video : STD_LOGIC_VECTOR(7 downto 0) := "00000000";
-    alias red : STD_LOGIC_VECTOR(2 downto 0) is video(7 downto 5);
-    alias green : STD_LOGIC_VECTOR(2 downto 0) is video(4 downto 2);
+    alias red : STD_LOGIC_VECTOR(2 downto 0) is video(6 downto 4);
+    alias green : STD_LOGIC_VECTOR(1 downto 0) is video(3 downto 2);
     alias blue : STD_LOGIC_VECTOR(1 downto 0) is video(1 downto 0);
     signal timer : STD_LOGIC_VECTOR(7 downto 0) := X"00";
 
@@ -30,19 +30,37 @@ architecture gpu_one of gpu is
     type tile_pixel_mem_type is array (0 to 31) of tile_pixel_data_type;
     constant tile_pixel_mem : tile_pixel_mem_type := 
         (
-             0 => (0 =>X"E0", others =>X"00"), -- röd
-             1 => (0 =>X"1C", others =>X"00"), -- grön
-             2 => (0 =>X"03", others =>X"00"), -- blå
-        others => (others =>X"00") -- svart
+             0 => (others =>X"00"), -- transparent
+             1 => (others =>X"AC"), -- grön
+             2 => (others =>X"A3"), -- blå
+             10 => (0 =>X"E0", 1 =>X"1C", 2 =>X"03", 3 =>X"A6",4 =>X"E0", 5 =>X"1C", 6 =>X"03", 7 =>X"A6",
+		           8 =>X"E0", 9 =>X"1C", 10 =>X"03", 11 =>X"A6",12 =>X"E0", 13 =>X"1C", 14 =>X"03",
+                   15 =>X"A6",30 =>X"E0", 31 =>X"1C", 42 =>X"03", 53 =>X"A6",64 =>X"E0", 75 =>X"1C", 
+                   86 =>X"03", 97 =>X"A6",108 =>X"E0", 119 =>X"1C", 110 =>X"03", 111 =>X"A6",112 =>X"E0",
+                   213 =>X"1C", 244 =>X"03", 150 =>X"A6", others =>X"01"),
+        others => (others =>X"00") -- transparent
         );
 
-    type tile_mem_row_type is array(0 to 50) of STD_LOGIC_VECTOR(7 downto 0);
-    type tile_mem_type is array(0 to 40) of tile_mem_row_type;
-    signal layer1_mem : tile_mem_type := (others => (others => "00000001"));
-    signal layer2_mem : tile_mem_type;
+    type tile_mem_row_type is array(0 to 99) of STD_LOGIC_VECTOR(7 downto 0);
+    type tile_mem_type is array(0 to 39) of tile_mem_row_type;
 
-    signal current_tile : STD_LOGIC_VECTOR(7 downto 0);
+    constant layer0_mem : tile_mem_type := (10 => (others =>X"02"), others => (10 => X"02", others => X"00"));
+    constant layer1_mem : tile_mem_type := (others => (others => X"0A"));
+    constant layer2_mem : tile_mem_type := (others => (others => X"00"));
+    constant layer3_mem : tile_mem_type := (others => (others => X"01"));
 
+    signal current_tile0 : STD_LOGIC_VECTOR(7 downto 0) := X"00";
+    signal current_tile1 : STD_LOGIC_VECTOR(7 downto 0) := X"00";
+    signal current_tile2 : STD_LOGIC_VECTOR(7 downto 0) := X"00";
+    signal current_tile3 : STD_LOGIC_VECTOR(7 downto 0) := X"00";
+
+
+    signal current_pixel0 : STD_LOGIC_VECTOR(7 downto 0) := X"00";
+    signal current_pixel1 : STD_LOGIC_VECTOR(7 downto 0) := X"00";
+    signal current_pixel2 : STD_LOGIC_VECTOR(7 downto 0) := X"00";
+    signal current_pixel3 : STD_LOGIC_VECTOR(7 downto 0) := X"00";
+
+    signal pipeline_count : STD_LOGIC_VECTOR(1 downto 0) := "00";
 begin
     -- Pixel clock
     process(clk) begin
@@ -58,18 +76,18 @@ begin
   process(clk) begin
     if rising_edge(clk) then
       if rst='1' then
-         xctr <= "0000000000";
+         xctr <= "00000000000";
       elsif pixel=3 then
        if xctr=799 then
-         xctr <= "0000000000";
+         xctr <= "00000000000";
        else
          xctr <= xctr + 1;
        end if;
       end if;
       --
-      if xctr=688 then
+      if xctr=656 then -- 688
         hs <= '0';
-      elsif xctr=784 then
+      elsif xctr=752 then -- 784
         hs <= '1';
       end if;
     end if;
@@ -78,17 +96,17 @@ begin
   process(clk) begin
     if rising_edge(clk) then
       if rst='1' then
-        yctr <= "0000000000";
+        yctr <= "00000000000";
       elsif xctr=799 and pixel=0 then
        if yctr=520 then
-         yctr <= "0000000000";
+         yctr <= "00000000000";
        else
          yctr <= yctr + 1;
        end if;
        --
-       if yctr=509 then
+       if yctr=490 then -- 509
          vs <= '0';
-       elsif  yctr=511 then
+       elsif  yctr=492 then --511
          vs <= '1';
        end if;
       end if;
@@ -97,33 +115,46 @@ begin
   Hsync <= hs;
   Vsync <= vs;
 
-  -- video
-  -- en ram ritas runt spelplanen
-  -- tycks medföra att AUTO funkar som avsett
-  process(clk) begin
-    if rising_edge(clk) then
-      if pixel=3 then
-        if xctr(3 downto 0) = "1111" then
-          current_tile <= layer1_mem(0)(conv_integer(ytile));
-          --current_tile <= layer1_mem(100)(0);
-          --current_tile <= "00000010";
-        end if;
 
-        if xctr>640 or yctr>480 then
+
+  process(clk) begin
+    current_tile0 <= layer0_mem(conv_integer(ytile))(conv_integer(xtile));
+    current_tile1 <= layer1_mem(conv_integer(ytile))(conv_integer(xtile));
+    current_tile2 <= layer2_mem(conv_integer(ytile))(conv_integer(xtile));
+    current_tile3 <= layer3_mem(conv_integer(ytile))(conv_integer(xtile));
+    if rising_edge(clk) then
+      --if pixel = "11" then
+        if xctr>639 or yctr>479 then
           video <= "00000000";
         --elsif xctr=0 or xctr=639 or yctr=0 or yctr=479 then
         --  video<="11111111";
         elsif yctr<480 and xctr<640 then
-          video <= tile_pixel_mem(conv_integer(current_tile))(conv_integer(tilexoff+tileyoff*16));
+            if pixel = "00" then
+              current_pixel0 <= tile_pixel_mem(conv_integer(current_tile0))(conv_integer(tileyoff & tilexoff));
+              current_pixel1 <= tile_pixel_mem(conv_integer(current_tile1))(conv_integer(tileyoff & tilexoff));
+            elsif pixel = "01" then
+              current_pixel2 <= tile_pixel_mem(conv_integer(current_tile2))(conv_integer(tileyoff & tilexoff));
+              current_pixel3 <= tile_pixel_mem(conv_integer(current_tile3))(conv_integer(tileyoff & tilexoff));
+            elsif pixel = "11" then
+                if current_pixel0(7) = '1' then
+                    video <= current_pixel0;
+                elsif current_pixel1(7) = '1' then
+                    video <= current_pixel1;
+                elsif current_pixel2(7) = '1' then
+                    video <= current_pixel2;
+                else
+                    video <= current_pixel3;
+                end if;
+            end if;
         else
           video <= "00000000";
         end if;
-      end if;
+      --end if;
     end if;
   end process;
 
   vgaRed(2 downto 0) <= (red);
-  vgaGreen(2 downto 0) <= (green(2 downto 1) & '0');
+  vgaGreen(2 downto 0) <= (green & '0');
   vgaBlue(2 downto 1) <= (blue);
 
 
